6674a193505048f2f94e7964d1d25dd9d1b5fe46
Dworkin3228 <xah3000@yandex.ru>
2022-04-07 20:04:14 +0300

    Добавил новое приложение для страницы менеджера

--- ticket_view/views.py
+++ ticket_view/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from contact_form.forms import *
 
 
 def ticketview(request):
-    return render(request, "ticket_view/ticket_view.html")
-
+    if request.method == 'POST':
+        tv = Contact.objects.all()
+        print(tv)
+    else:
+        tv = Contact.objects.all()
+        print(tv)
+        return render(request, "ticket_view/ticket_view.html")
